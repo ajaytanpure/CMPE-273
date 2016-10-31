ef6b21c30d702d5432f82f15b1ce71b1043161d2
Ajay <ajay.tanpure@sjsu.edu>
2016-10-31 14:18:59 -0700

    removed commented old code

--- Demos/RabbitMQ/RabbitMQ-Server/services/register.js
+++ Demos/RabbitMQ/RabbitMQ-Server/services/register.js
@@ -28,28 +28,3 @@ function handle_request(msg, callback){
 }
 
 exports.handle_request = handle_request;
-
-
-
-// exports.register = function(req, res){
-// 	var username = req.body['username'];
-// 	var password = req.body['password'];
-// 	var firstname = req.body['firstname'];
-// 	var lastname = req.body['lastname'];
-// 	var mobile = req.body['mobile'];
-	
-// 	mongo.connect(mongoURL, function(){
-// 		console.log('Connected to mongo at: ' + mongoURL);
-// 		var coll = mongo.collection('newCollection');
-
-// 		coll.insertOne({username:username, password :password, firstName:firstname,lastName:lastname, mobile:mobile }, function(err, user){
-// 			if (user) {
-// 				console.log(user);
-// 				res.send({'status':true});
-// 			} else {
-// 				console.log("returned false");
-// 				res.send({'status':false})
-// 			}
-// 		});		
-// 	});
-// }
