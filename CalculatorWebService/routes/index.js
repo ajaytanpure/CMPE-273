c177d680931ef487d9a864efa764617c4d4c9ded
Ajay <ajay.tanpure@sjsu.edu>
2016-12-08 15:34:49 -0800

    Soap Service for calculator

--- CalculatorWebService/routes/index.js
+++ CalculatorWebService/routes/index.js
@@ -2,79 +2,88 @@
  * GET home page.
  */
 
-exports.index = function(req, res) {
-	res.render('calculator.ejs');
-};
+ var soap = require('soap');
+ var baseURL = "http://localhost:8080/SOAPCalculator/services";
 
-// exports.calculate = function(req, res) {
-// var exp = req.body['exp'];
-// console.log("Exprssion from client : " + exp);
-// var result = {
-// 'val' : '',
-// 'valid' : true,
-// 'msg' : ''
-// };
-//	
-// try {
-// result['val'] = eval(exp);
-// console.log("Result : " + result['val']);
-// if (result['val'] == Infinity) {
-// result['msg'] = 'Divide By Zero Error';
-// throw "DivideByZeroError"
-// }
-// res.send(result);
-// } catch (err) {
-// console.log(err);
-// result['valid'] = false;
-// if (result['msg'] == '') {
-// result['msg'] = 'Please Check The Expression Syntax';
-// }
-// res.send(result);
-// }
-//
-// };
 
-exports.calculate = function(req, res) {
-	var op1 = req.body['op1'];
-	var op2 = req.body['op2'];
-	var op = req.body['operator'];
-	var result = {
-		'val' : '',
-		'status' : ''
-	}
-	console.log(op1 + " " + op + " " + op2);
-	try {
-		if (op === '+') {
-			var tmp = parseFloat(op1) + parseFloat(op2);
-			result['val'] = tmp.toString();
-			console.log(result['val']);
-			res.send(result);
-		} else if (op === '-') {
-			var tmp = parseFloat(op1) - parseFloat(op2);
-			result['val'] = tmp.toString();
-			console.log(result['val']);
-			res.send(result);
-		} else if (op === '*') {
-			var tmp = parseFloat(op1) * parseFloat(op2);
-			result['val'] = tmp.toString();
-			console.log(result['val']);
-			res.send(result);
-		} else if (op === '/') {
+ exports.index = function(req, res) {
+ 	res.render('calculator.ejs');
+ };
 
-			console.log(typeof (op2));
-			if (op2 == '0') {
-				console.log('In division');
-				throw 'Divide By Zero error'
-			}
-			;
-			var tmp = parseFloat(op1) / parseFloat(op2);
-			result['val'] = tmp.toString();
-			console.log(result['val'])
-			res.send(result);
-		}
-	} catch (err) {
-		console.log('in exception');
-		result['status'] = err;
-		res.send(result);
-	}
-};
+ exports.calculate = function(req, res) {
+ 	var option = {
+ 		ignoredNamespaces : true	
+ 	};
+
+ 	var op1 = req.body['op1'];
+ 	var op2 = req.body['op2'];
+ 	var op = req.body['operator'];
+ 	var result = {
+ 		'val' : '',
+ 		'status' : ''
+ 	}
+
+ 	var url = baseURL+"/Calculate?wsdl";
+ 	var args = {"value":3}
+
+ 	console.log(op1 + " " + op + " " + op2);
+ 	try {
+ 		if (op === '+') {
+ 			var args = {"op1":op1, "operator":"+", "op2":op2}
+ 			soap.createClient(url,option, function(err, client) {
+ 				console.log(err)
+ 				client.calculate(args, function(err, result1) {
+ 					console.log(result1.calculateReturn);
+ 					result['val'] = result1.calculateReturn.toString();
+ 					console.log(result['val']);
+ 					res.send(result);
+ 				});
+ 			});
+
+ 		} else if (op === '-') {
+ 			var args = {"op1":op1, "operator":"-", "op2":op2}
+ 			soap.createClient(url,option, function(err, client) {
+ 				console.log(err)
+ 				client.calculate(args, function(err, result1) {
+ 					console.log(result1.calculateReturn);
+ 					result['val'] = result1.calculateReturn.toString();
+ 					console.log(result['val']);
+ 					res.send(result);
+ 				});
+ 			});
+ 		} else if (op === '*') {
+ 			var args = {"op1":op1, "operator":"*", "op2":op2}
+ 			soap.createClient(url,option, function(err, client) {
+ 				console.log(err)
+ 				client.calculate(args, function(err, result1) {
+ 					console.log(result1.calculateReturn);
+ 					result['val'] = result1.calculateReturn.toString();
+ 					console.log(result['val']);
+ 					res.send(result);
+ 				});
+ 			});
+ 		} else if (op === '/') {
+
+ 			console.log(typeof (op2));
+ 			if (op2 == '0') {
+ 				console.log('In division');
+ 				throw 'Divide By Zero error'
+ 			}
+ 			;
+ 			var args = {"op1":op1, "operator":"/", "op2":op2}
+ 			soap.createClient(url,option, function(err, client) {
+ 				console.log(err)
+ 				client.calculate(args, function(err, result1) {
+ 					console.log(result1.calculateReturn);
+ 					result['val'] = result1.calculateReturn.toString();
+ 					console.log(result['val']);
+ 					res.send(result);
+ 				});
+ 			});
+ 		}
+ 	} catch (err) {
+ 		console.log('in exception');
+ 		result['status'] = err;
+ 		res.send(result);
+ 	}
+ };
