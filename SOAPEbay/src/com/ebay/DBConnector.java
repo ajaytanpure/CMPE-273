9b64b69810c23aeb1b068f8678ce564bf5baecea
Ajay <ajay.tanpure@sjsu.edu>
2016-12-08 19:25:34 -0800

    Connection pooling

--- SOAPEbay/src/com/ebay/DBConnector.java
+++ SOAPEbay/src/com/ebay/DBConnector.java
@@ -2,23 +2,38 @@ package com.ebay;
 
 import java.sql.*;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.ConnectionPoolDataSource;
+
 public class DBConnector {
 
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	static final String DB_URL = "jdbc:mysql://localhost:3306/StartTest";
 	static final String USER = "root";
 	static final String PASSWORD = "root123";
-
+	Connection conn = null;
 	
-	public Connection connect(){
-		Connection conn = null;
+	public Connection getConnector() {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		    System.out.println("Connecting to Database");
-		    conn = DriverManager.getConnection(DB_URL,USER,PASSWORD);	
-		} catch (Exception e) {
+			Context ctx = new InitialContext();
+			ConnectionPoolDataSource cpds = (ConnectionPoolDataSource)ctx.lookup("");
+			((Object) cpds).setDatabaseName("StartTest"); 
+            ((Object) cpds).setUserIF("root");
+            PooledConnection pc = cpds.getPooledConnection(); 
+            Connection conn = pc.getConnection(); 
+		} catch (NamingException e) {
 			e.printStackTrace();
 		}
-		return conn;		
+		return conn;
+	}
+	
+	public void closeConnection(){
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
